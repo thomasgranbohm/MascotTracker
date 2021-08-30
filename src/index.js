5b45a6aeb15ee8220a6ff4c0c7188e707e8382db
Thomas Granbohm <thomas.granbohm@gmail.com>
2021-08-31 00:27:05 +0200

    🧹 Formatting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-const { spreadsheet_id, webhook_link, api_key } = require("../config.json")
-const { GoogleSpreadsheet } = require("google-spreadsheet");
 const axios = require("axios").default;
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-const parseMascotData = (rows) => {
-	return rows.map((row) => ({
+const { spreadsheet_id, webhook_link, api_key } = require("../config.json");
+
+let prevOnline = [],
+	prevOffline = [];
+
+const parseMascotData = (rows) =>
+	rows.map((row) => ({
 		name: row.Mascot,
 		room: row.Room,
 		server: row["Enter the server below:"],
 	}));
-};
 
 const generateEmbed = (mascot, online = false) => ({
-	title: `${mascot.name} went ${online ? `online in ${mascot.server}!`: "offline."}`,
+	title: `${mascot.name} went ${
+		online ? `online in ${mascot.server}!` : "offline."
+	}`,
 	url: online ? "https://play.cprewritten.net" : null,
 	footer: {
 		text: online ? `🌎 ${mascot.server} 🏠 ${mascot.room}` : null,
@@ -20,20 +25,16 @@ const generateEmbed = (mascot, online = false) => ({
 	timestamp: new Date().toISOString(),
 });
 
-const executeWebhook = (data) => axios.post(webhook_link, data, {
-	headers: {
-		"Content-Type": "application/json"
-	}
-});
-
-let prevOnline = [],
-	prevOffline = [];
+const executeWebhook = (data) =>
+	axios.post(webhook_link, data, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 
 const main = async () => {
-	console.log("Running!")
-	const doc = new GoogleSpreadsheet(
-		spreadsheet_id
-	);
+	console.log("Running!");
+	const doc = new GoogleSpreadsheet(spreadsheet_id);
 
 	await doc.useApiKey(api_key);
 
@@ -45,20 +46,25 @@ const main = async () => {
 		try {
 			rows = await sheet.getRows();
 		} catch (err) {
-			await executeWebhook({content: "I am getting rate limited"})
-			return process.exit();
+			await executeWebhook({ content: "I am getting rate limited" });
+			return;
 		}
 
-		const mascots =  parseMascotData(rows);
-		
-		if (prevOffline.length === 0 && prevOnline.length === 0) prevOffline = mascots;
+		const mascots = parseMascotData(rows);
+
+		if (prevOffline.length === 0 && prevOnline.length === 0)
+			prevOffline = mascots;
 
 		const online = mascots
-			.filter((o) => o.server !== "" && o.server !== "Offline")
-			.filter((o) => !prevOnline.find((mascot) => mascot.name === o.name)),
+				.filter((o) => o.server !== "" && o.server !== "Offline")
+				.filter(
+					(o) => !prevOnline.find((mascot) => mascot.name === o.name)
+				),
 			offline = mascots
-			.filter((o) => o.server === "Offline")
-			.filter((o) => !prevOffline.find((mascot) => mascot.name === o.name));
+				.filter((o) => o.server === "Offline")
+				.filter(
+					(o) => !prevOffline.find((mascot) => mascot.name === o.name)
+				);
 
 		const embeds = [
 			...offline.map((mascot) => generateEmbed(mascot)),
@@ -67,23 +73,32 @@ const main = async () => {
 
 		if (embeds.length === 0) return;
 
-		let date = new Date();
-		offline.map((mascot) => `${date.toISOString()} ${mascot.name} went offline.`).forEach(console.log)
-		online.map((mascot) => `${date.toISOString()} ${mascot.name} went online in ${mascot.server}!`).forEach(console.log)
+		const date = new Date();
+		[
+			...offline.map(
+				(mascot) => `${date.toISOString()} ${mascot.name} went offline.`
+			),
+			...online.map(
+				(mascot) =>
+					`${date.toISOString()} ${mascot.name} went online in ${
+						mascot.server
+					}!`
+			),
+		].forEach(console.log);
 
 		try {
-			await executeWebhook({
-				embeds: embeds,
-			})
+			await executeWebhook({ embeds });
 		} catch (err) {
-			console.error(err.response.data)
+			console.error("Error executing webhook:", err.response.data);
 		}
 
-		prevOnline = mascots.filter((o) => o.server !== "" && o.server !== "Offline");
-		prevOffline = mascots.filter((o) => o.server === "Offline")
-	}
+		prevOnline = mascots.filter(
+			(o) => o.server !== "" && o.server !== "Offline"
+		);
+		prevOffline = mascots.filter((o) => o.server === "Offline");
+	};
 
-	setInterval(run, 2e3)
+	setInterval(run, 2e3);
 	run();
 };
 
