2b5c8542f4d271ed52284abbbac973e8c62c462e
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-02-03 14:33:03 +0100

    Updated logging capabilities

--- src/index.ts
+++ src/index.ts
@@ -5,262 +5,279 @@ import { Embed, Mascot, OnlineType, WebhookMessage } from "types";
 import { format, promisify } from "util";
 
 namespace Logger {
-  export const log = (message?: any, ...optionalParams: any[]) =>
-    console.log(
-      blue(new Date().toLocaleString("sv")),
-      format(message, ...optionalParams)
-    );
-  export const error = (message?: any, ...optionalParams: any[]) =>
-    console.error(
-      red(new Date().toLocaleString("sv")),
-      format(message, ...optionalParams)
-    );
+	export const log = (message?: any, ...optionalParams: any[]) =>
+		console.log(
+			blue(new Date().toLocaleString("sv")),
+			format(message, ...optionalParams)
+		);
+	export const debug = (message?: any, ...optionalParams: any[]) =>
+		process.env.NODE_ENV !== "production" &&
+		console.debug(
+			green(new Date().toLocaleString("sv")),
+			format(message, ...optionalParams)
+		);
+	export const error = (message?: any, ...optionalParams: any[]) =>
+		console.error(
+			red(new Date().toLocaleString("sv")),
+			format(message, ...optionalParams)
+		);
 }
 
 const { API_KEY, SPREADSHEET_ID, WEBHOOK_LINK } = process.env;
 if (!API_KEY || !SPREADSHEET_ID || !WEBHOOK_LINK) {
-  Logger.log("Please fill in the every environment variable before running!");
-  process.exit(1);
+	Logger.error(
+		"Please fill in the every environment variable before running!"
+	);
+	process.exit(1);
 }
 
 const sleep = promisify(setTimeout);
 
 process.on("beforeExit", (code) => {
-  if (code !== 0) {
-    executeWebhook({
-      content: "I'm dying lmao...",
-    })
-      .catch(() => Logger.error("Could not send error webhook."))
-      .finally(() => process.exit(code));
-  } else {
-    process.exit(code);
-  }
+	if (code !== 0) {
+		executeWebhook({
+			content: "I'm dying lmao...",
+		})
+			.catch(() => Logger.error("Could not send error webhook."))
+			.finally(() => process.exit(code));
+	} else {
+		process.exit(code);
+	}
 });
 
 process.on("exit", (code) => Logger.log("Exited with code %d", code));
 
 const generateEmbed = (key: string, value: OnlineType): Embed => ({
-  title: `${key} went ${!!value ? `online in ${value[0]}` : "offline."}`,
-  url: !!value ? "https://play.cprewritten.net" : undefined,
-  color: !!value ? 5814783 : 16074818,
-  timestamp: new Date(),
-  footer: {
-    text: !!value
-      ? `🌍 ${value[0]}${value[1] ? ` 🏡 ${value[1]}` : ""}`
-      : undefined,
-  },
+	title: `${key} went ${!!value ? `online in ${value[0]}` : "offline."}`,
+	url: !!value ? "https://play.cprewritten.net" : undefined,
+	color: !!value ? 5814783 : 16074818,
+	timestamp: new Date(),
+	footer: {
+		text: !!value
+			? `🌍 ${value[0]}${value[1] ? ` 🏡 ${value[1]}` : ""}`
+			: undefined,
+	},
 });
 
 const executeWebhook = async (
-  data: WebhookMessage,
-  options?: AxiosRequestConfig
+	data: WebhookMessage,
+	options?: AxiosRequestConfig
 ) => {
-  try {
-    const resp = await axios({
-      url: WEBHOOK_LINK,
-      method: "post",
-      ...options,
-      data,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+	try {
+		const resp = await axios({
+			url: WEBHOOK_LINK,
+			method: "post",
+			...options,
+			data,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-    return resp.data;
-  } catch (error: any) {
-    if (error.isAxiosError && error.response) {
-      error("Error when executing webhook: %o", error.response);
-    } else {
-      error("Got regular error: %o", error);
-    }
-  }
+		return resp.data;
+	} catch (err: any) {
+		if (err.isAxiosError && err.response) {
+			Logger.error("Error when executing webhook: %o", err.response);
+		} else {
+			Logger.error("Got regular error: %o", err);
+		}
+	}
 };
 
 const sendMessage = async (key: string, value: OnlineType): Promise<string> => {
-  const data = await executeWebhook(
-    {
-      embeds: [generateEmbed(key, value)],
-    },
-    {
-      params: {
-        wait: true,
-      },
-    }
-  );
+	const data = await executeWebhook(
+		{
+			embeds: [generateEmbed(key, value)],
+		},
+		{
+			params: {
+				wait: true,
+			},
+		}
+	);
 
-  return data.id;
+	return data.id;
 };
 
 const updateMessage = async (key: string, value: OnlineType) => {
-  const messageId = embeds.get(key);
-  if (!messageId) throw new Error(red("Could not find messageId with " + key));
+	const messageId = embeds.get(key);
+	if (!messageId)
+		throw new Error(red("Could not find messageId with " + key));
 
-  const data = await executeWebhook(
-    {
-      embeds: [generateEmbed(key, value)],
-    },
-    {
-      url: `${WEBHOOK_LINK}/messages/${messageId}`,
-      method: "patch",
-    }
-  );
+	const data = await executeWebhook(
+		{
+			embeds: [generateEmbed(key, value)],
+		},
+		{
+			url: `${WEBHOOK_LINK}/messages/${messageId}`,
+			method: "patch",
+		}
+	);
 
-  return data;
+	return data;
 };
 
 const compareValues = (newValue: OnlineType, oldValue: OnlineType) => {
-  if (newValue === undefined && oldValue === undefined) return true;
-  if (typeof newValue !== "undefined" && typeof oldValue !== "undefined") {
-    if (
-      newValue.length === 1 &&
-      oldValue.length === 1 &&
-      newValue[0] === oldValue[0]
-    )
-      return true;
-    if (
-      newValue.length === 2 &&
-      oldValue.length === 2 &&
-      newValue[0] === oldValue[0] &&
-      newValue[1] === oldValue[1]
-    )
-      return true;
-  }
-  return false;
+	if (newValue === undefined && oldValue === undefined) return true;
+	if (typeof newValue !== "undefined" && typeof oldValue !== "undefined") {
+		if (
+			newValue.length === 1 &&
+			oldValue.length === 1 &&
+			newValue[0] === oldValue[0]
+		)
+			return true;
+		if (
+			newValue.length === 2 &&
+			oldValue.length === 2 &&
+			newValue[0] === oldValue[0] &&
+			newValue[1] === oldValue[1]
+		)
+			return true;
+	}
+	return false;
 };
 
 const printMascot = (
-  name: string,
-  oldValue: OnlineType,
-  newValue: OnlineType
+	name: string,
+	oldValue: OnlineType,
+	newValue: OnlineType
 ) => {
-  const parseOnlineValue = (o: OnlineType) => {
-    if (o === undefined) return "Offline";
-    if (o.length === 2 && o[1]) return `${o[1]}, ${o[0]}`;
-    if (o.length === 1 || (o.length === 2 && !o[1])) return o[0];
-    return o.toString();
-  };
-  return `${bold.white(name)}: ${red(parseOnlineValue(oldValue))} -> ${green(
-    parseOnlineValue(newValue)
-  )}`;
+	const parseOnlineValue = (o: OnlineType) => {
+		if (o === undefined) return "Offline";
+		if (o.length === 2 && o[1]) return `${o[1]}, ${o[0]}`;
+		if (o.length === 1 || (o.length === 2 && !o[1])) return o[0];
+		return o.toString();
+	};
+	return `${bold.white(name)}: ${red(parseOnlineValue(oldValue))} -> ${green(
+		parseOnlineValue(newValue)
+	)}`;
 };
 
 const getChanges = async () => {
-  for await (const [key, newValue] of collection) {
-    const oldValue = prevCollection.get(key);
+	for await (const [key, newValue] of collection) {
+		const oldValue = prevCollection.get(key);
 
-    if (compareValues(newValue, oldValue)) continue;
+		if (compareValues(newValue, oldValue)) continue;
 
-    prevCollection.set(key, newValue);
+		prevCollection.set(key, newValue);
 
-    // Print change message
-    Logger.log(printMascot(key, oldValue, newValue));
+		// Print change message
+		Logger.log(printMascot(key, oldValue, newValue));
 
-    // Just went online or offline. e.g. send a new message
-    if (newValue === undefined || oldValue === undefined) {
-      const id = await sendMessage(key, newValue);
+		// Just went online or offline. e.g. send a new message
+		if (newValue === undefined || oldValue === undefined) {
+			const id = await sendMessage(key, newValue);
 
-      if (newValue === undefined) embeds.delete(key);
-      if (oldValue === undefined) embeds.set(key, id);
-    }
-    if (oldValue !== undefined && newValue !== undefined) {
-      await updateMessage(key, newValue);
-    }
-  }
+			if (newValue === undefined) embeds.delete(key);
+			if (oldValue === undefined) embeds.set(key, id);
+		}
+		if (oldValue !== undefined && newValue !== undefined) {
+			await updateMessage(key, newValue);
+		}
+	}
 };
 
 const updateCollections = (key: string, newValue: OnlineType) => {
-  const oldValue = collection.get(key);
+	const oldValue = collection.get(key);
 
-  if (compareValues(newValue, oldValue)) return;
+	if (compareValues(newValue, oldValue)) return;
 
-  prevCollection.set(key, oldValue);
+	prevCollection.set(key, oldValue);
 
-  collection.set(key, newValue);
+	collection.set(key, newValue);
 };
 
 const parseMascotData = (rows: GoogleSpreadsheetRow[]): Mascot[] =>
-  rows
-    .filter((row) => {
-      if (row["Mascot"]) return row;
-    })
-    .map(
-      (row): Mascot => ({
-        name: row["Mascot"],
-        online:
-          row["Enter the server below:"] !== "Offline"
-            ? row["Enter room override below:"].startsWith("Last visited")
-              ? [row["Enter the server below:"]]
-              : [
-                  row["Enter the server below:"],
-                  row["Enter room override below:"],
-                ]
-            : undefined,
-      })
-    )
-    .filter((mascot) => mascot.name !== "");
+	rows
+		.filter((row) => {
+			if (row["Mascot"]) return row;
+		})
+		.map(
+			(row): Mascot => ({
+				name: row["Mascot"],
+				online:
+					"Enter the server below:" in row &&
+					!!row["Enter the server below:"] &&
+					row["Enter the server below:"] !== "Offline"
+						? row["Enter room override below:"].startsWith(
+								"Last visited"
+						  )
+							? [row["Enter the server below:"]]
+							: [
+									row["Enter the server below:"],
+									row["Enter room override below:"],
+							  ]
+						: undefined,
+			})
+		)
+		.filter((mascot) => mascot.name !== "");
 
 const collection = new Map<string, OnlineType>();
 const prevCollection = new Map<string, OnlineType>(collection);
 const embeds = new Map<string, string>();
 
 const main = async () => {
-  enable();
+	enable();
 
-  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+	const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
-  await doc.useApiKey(API_KEY);
-  await doc.loadInfo();
+	await doc.useApiKey(API_KEY);
+	await doc.loadInfo();
 
-  let rateLimited = false;
+	let rateLimited = false;
 
-  const sheet = doc.sheetsByIndex[0];
+	const sheet = doc.sheetsByIndex[0];
 
-  const run = async () => {
-    let rows = [];
-    try {
-      rows = await sheet.getRows();
-    } catch (error: any) {
-      if (error.isAxiosError && error.response) {
-        if (error.response.status === 429) {
-          if (!rateLimited) {
-            Logger.log("Rate-limit Error");
-            await executeWebhook({
-              content: "I am getting rate limited",
-            });
-            rateLimited = true;
-          }
-        } else if (
-          error.response.status === 503 ||
-          error.response.status === 502
-        ) {
-          Logger.log("Temporary Google API Error %d", error.response.status);
-        }
-        Logger.error(JSON.stringify(error.response.data, null, 4));
-      } else {
-        Logger.error("Error while getting rows!");
-        Logger.error(error);
-      }
+	const run = async () => {
+		let rows = [];
+		try {
+			rows = await sheet.getRows();
+		} catch (err: any) {
+			if (err.isAxiosError && err.response) {
+				if (err.response.status === 429) {
+					if (!rateLimited) {
+						Logger.error("Rate-limit Error");
+						await executeWebhook({
+							content: "I am getting rate limited",
+						});
+						rateLimited = true;
+					}
+				} else if (
+					err.response.status === 503 ||
+					err.response.status === 502
+				) {
+					Logger.debug(
+						"Temporary Google API Error %d",
+						err.response.status
+					);
+				} else {
+					Logger.error(JSON.stringify(err.response.data, null, 4));
+				}
+			} else {
+				Logger.error("Error while getting rows!");
+				Logger.error(err);
+			}
 
-      await sleep(5e3);
+			await sleep(5e3);
 
-      return;
-    }
+			return;
+		}
 
-    const mascots = parseMascotData(rows);
-    for (const { name, online } of mascots) {
-      updateCollections(name, online);
-    }
+		const mascots = parseMascotData(rows);
+		for (const { name, online } of mascots) {
+			updateCollections(name, online);
+		}
 
-    getChanges();
-  };
+		getChanges();
+	};
 
-  const waitOnFunction = async (f: Function, delay = 5e3): Promise<void> => {
-    await f();
-    await sleep(delay);
-    return waitOnFunction(f, delay);
-  };
-  waitOnFunction(run);
-  Logger.log(bold("Started!"));
+	const waitOnFunction = async (f: Function, delay = 5e3): Promise<void> => {
+		await f();
+		await sleep(delay);
+		return waitOnFunction(f, delay);
+	};
+	waitOnFunction(run);
+	Logger.log(bold("Started!"));
 };
 
 main();
